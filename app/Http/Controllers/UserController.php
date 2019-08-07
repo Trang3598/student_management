4c4d272ada4cfd9f3f6869b6af7978f37e35db7c
WashingtonDC <leduccuong.neu@gmail.com>
2019-08-07 09:51:07 +0700

    up 07/08 morning

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PasswordRequest;
+use App\Http\Requests\UserEditRequest;
+use App\Repositories\User\UserRepository;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    protected $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function edit($id)
+    {
+        $user = $this->userRepository->getListById($id);
+
+        return view('admin.users.edit',compact('user'));
+    }
+    public function update(UserEditRequest $request, $id)
+    {
+        $this->userRepository->update($id, $request->all());
+
+        return redirect()->back()->with(['success' => 'Update success']);
+    }
+    public function change($id)
+    {
+        $user = $this->userRepository->getListById($id);
+        return view('admin.users.change',compact('user'));
+    }
+    public function changed($id, PasswordRequest $request)
+    {
+/*        $user = $this->userRepository->getListById($id);
+        $oldpassword = $user['password'];
+        $data = $request->all();
+        dd($data['password_old']);*/
+        return view('admin.users.change',compact('user'));
+    }
+}
---
+++ app/Http/Requests/UserEditRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserEditRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'user' => 'required|unique:users,user',
+            'email' => 'required|unique:users,email',
+        ];
+    }
+
+    public function messages()
+    {
+        return[
+            'user.unique'=>'Same User',
+            'email.required'=>'No Email',
+            'email.unique' => 'Same Email',
+        ];
+    }
+}
