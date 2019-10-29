e954c0fb729adb78b4bd9abc6d67254d2cc86721
Trang3598 <nguyenthikieutrang_t61@hus.edu.vn>
2019-10-29 17:48:18 +0700

    sql

--- quanlysinhvien.sql
+++ quanlysinhvien.sql
@@ -1,32 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 29, 2019 lúc 11:24 AM
--- Phiên bản máy phục vụ: 10.3.16-MariaDB
--- Phiên bản PHP: 7.2.19
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Cơ sở dữ liệu: `test1`
---
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `classes`
---
 
 CREATE TABLE `classes` (
   `id` bigint(20) UNSIGNED NOT NULL,
